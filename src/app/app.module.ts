6ac57fbe1b1f37197b616ddd7a527921e7be24fa
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-11-06 12:48:59 +0000

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {PizzalistComponent} from './pizzalist/pizzalist.component';
+import {BasketComponent} from './basket/basket.component';
+import {BasketService} from './basket.service';
+import {PizzaService} from './pizza.service';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, HeaderComponent, PizzalistComponent, BasketComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  exports: [],
+  providers: [BasketService, PizzaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/pizzalist/pizzalist.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener
+} from '@angular/core';
+import {PIZZAS} from '../pizzasList';
+import {BasketComponent} from '../basket/basket.component';
+import {BasketService} from '../basket.service';
+import {Pizza} from '../pizza';
+
+@Component({
+  selector: 'app-pizzalist',
+  templateUrl: './pizzalist.component.html',
+  styleUrls: ['./pizzalist.component.css']
+})
+export class PizzalistComponent implements OnInit {
+  constructor(private basketService: BasketService) {
+  }
+
+  // @Input() name: string;
+  @Output() isAdded = new EventEmitter<boolean>();
+  pizzas = PIZZAS;
+  pizzaList: Pizza[] = [];
+
+  ngOnInit() {
+  }
+
+  updateList(pizza: Pizza, addedToTotal: boolean) {
+    this.basketService.addToTotalAmount(pizza.price, addedToTotal);
+    this.isAdded.emit(addedToTotal);
+  }
+
+  decrementNumber(pizza: Pizza) {
+    // Decrement the number of the ordered pizza
+    // the total amount of the selected pizza should be reduced as well
+    // call the update list
+  }
+
+  incrementNumber(pizza: Pizza) {
+    // Increment the number of the ordered pizza
+    // the total amount of the selected pizza should be augmented as well
+    // call the update list
+  }
+}
